c0237a943cf28e2ef58f4c615e5b497ae33f54a1
Nick Thompson <dev@nickthompson.guru>
2018-06-12 19:24:34 -0400

    Initial Commit

---
+++ Pipit/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Pipit
+//
+//  Created by Nick Thompson on 6/12/18.
+//  Copyright © 2018 Nick Thompson. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Pipit/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Pipit
+//
+//  Created by Nick Thompson on 6/12/18.
+//  Copyright © 2018 Nick Thompson. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
